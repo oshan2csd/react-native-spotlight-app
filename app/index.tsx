f30d29e81fe1da137dd39d1ac9c1711085c950a0
Oshan Silva <oshans@timbertruss.com.au>
2025-05-24 01:10:16 +1000

    Initial commit - removed boilerplate code - added a simple Hello text to the UI

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { styles } from "../styles/auth.styles.js";
+
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello123</Text>
+    </View>
+  );
+}
---
+++ styles/auth.styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",            
+      },
+      title: {
+        fontSize: 50,
+        textAlign: "center",
+        margin: 10,
+        color: "blue",
+      },
+    });
