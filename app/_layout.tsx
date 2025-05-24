01edda5b92186f642990e1ef02aaf8df88aeb09f
Oshan Silva <oshans@timbertruss.com.au>
2025-05-24 19:50:39 +1000

    Clerk-expo authentication basic set up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return(
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>        
-        <Stack screenOptions={{ headerShown: false }}>/* All screens will be wrapped in SafeAreaView by using 'screenOptions' */
-        </Stack>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>        
+          <Stack screenOptions={{ headerShown: false }}>/* All screens will be wrapped in SafeAreaView by using 'screenOptions' */
+          </Stack>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkProvider>
   );
 }
